b07031d2ebc69cd6e57d1c205e18cc39803747ac
Alphonsine Rasoanomenjanahary <rasoanomenjanaharyalphonsine@gmail.com>
2023-10-11 10:48:37 +0300

    Creation homepage, Creation entity, validation entity

---
+++ src/Entity/Product.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    #[Assert\NotBlank]
+    #[Assert\Length( min: 2 )]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    #[Assert\PositiveOrZero]
+    private ?float $quantity_stock = null;
+
+    #[ORM\Column]
+    #[Assert\NotNull]
+    #[Assert\Positive]
+    private ?float $price = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $types = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $gender = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getQuantityStock(): ?float
+    {
+        return $this->quantity_stock;
+    }
+
+    public function setQuantityStock(float $quantity_stock): static
+    {
+        $this->quantity_stock = $quantity_stock;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getTypes(): ?string
+    {
+        return $this->types;
+    }
+
+    public function setTypes(string $types): static
+    {
+        $this->types = $types;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): static
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+}
